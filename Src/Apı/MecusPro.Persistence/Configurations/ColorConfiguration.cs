42b8c78bee141908b51664f1f04e41a2fb9c37f9
ilhan enes daniş <danisenes3@gmail.com>
2023-08-29 23:18:56 +0300

    colorConfiguration

---
+++ Src/Apı/MecusPro.Persistence/Configurations/ColorConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using MecusPro.Domain.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MecusPro.Persistence.Configurations
+{
+    public class ColorConfiguration:BaseConfigurations<Color>
+    {
+        public override void Configure(EntityTypeBuilder<Color> builder)
+        {
+            base.Configure(builder);
+
+            builder.Property(x => x.ColorName).IsRequired();
+            
+            builder.HasOne(x => x.Languages).WithMany(x => x.Colors).HasForeignKey(x => x.LanguageId);
+            
+        }
+    }
+}
